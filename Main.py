38116a35568ecfaee29761ece703b595b894993c
Léo <49564283+Haxy972@users.noreply.github.com>
2022-12-31 20:56:37 +0100

    Update of Main.py :100:

--- Main.py
+++ Main.py
@@ -1,27 +1,46 @@
 import string
 import random
-
-password_file = open("cache/password.yml", "a+")
-
-
-def get_random_password(length: int = 16):
-    result = ""
-    main_list = string.ascii_letters + string.punctuation + string.digits
-    for _ in range(length):
-        index = random.randint(0, len(main_list) - 1)
-        result += main_list[index]
-
-    return result
+import PasswordManager as passwordManager
 
 
-def register_password(application_name: str, password: str):
-    global password_file
-    text = "\n" + application_name + ": \"" + password + "\""
-    password_file.write(text)
+def create_password():
+    application = str(input("Vous voulez enregistrer un mot de passe pour quelle application: ")).lower()
+    if not passwordManager.contain_app(application):
+        password = passwordManager.get_random_password()
+        passwordManager.register_password(application, password)
+        print(application + ": " + password)
+    else:
+        print("Vous avez déjà enregistré un mot de passe pour cette application")
 
 
-application = str(input("Vous voulez enregistrer un mot de passe pour quelle application: "))
-password = get_random_password()
-register_password(application, password)
-print(application + ": " + password)
+def see_all_applications():
+    print("\nListe des mots de passe enregistrés")
+    for application in passwordManager.get_all_applications():
+        print("- " + application)
+    print("\n")
 
+
+print("\n[0] : Générer un nouveau mot de passe")
+print("[1] : Voir tous les mots de passe")
+print("[2] : Regarder un mot de passe")
+print("[3] : Supprimer un mot de passe")
+print("[4] : Quitter l'application\n")
+password_file = passwordManager.get_password_file()
+loop = True
+while loop is True:
+    choice = input("Sélection: ")
+    if choice == "0":
+        create_password()
+    elif choice == "1":
+        see_all_applications()
+    elif choice == "2":
+        print("\nBientôt")
+    elif choice == "3":
+        print("\nBientôt")
+        # delete_password()
+    elif choice == "4":
+        print("Au revoir !")
+        loop = False
+
+    else:
+        print("Vous n'avez pas choisis un bon chiffre veuillez réessayer !")
