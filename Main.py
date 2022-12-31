f7d9f299f8126e783aa98cb63d41837461a7960d
Léo <49564283+Haxy972@users.noreply.github.com>
2022-12-31 17:25:58 +0100

    Update of Main.py :train:

--- Main.py
+++ Main.py
@@ -1,26 +1,27 @@
-
 import string
 import random
 
-password_file = open("cache/password.yml")
+password_file = open("cache/password.yml", "a+")
 
 
-def get_random_password(length : int = 16):
+def get_random_password(length: int = 16):
     result = ""
     main_list = string.ascii_letters + string.punctuation + string.digits
     for _ in range(length):
-        index = random.randint(0,len(main_list) - 1)
+        index = random.randint(0, len(main_list) - 1)
         result += main_list[index]
-    
+
     return result
-    
-def register_password(application_name : str,password : str):
+
+
+def register_password(application_name: str, password: str):
     global password_file
-    text = application_name +": \"" + password + "\""
+    text = "\n" + application_name + ": \"" + password + "\""
     password_file.write(text)
-    
-    
+
+
 application = str(input("Vous voulez enregistrer un mot de passe pour quelle application: "))
-password =  get_random_password()
+password = get_random_password()
 register_password(application, password)
-print(application +": "+ password)
+print(application + ": " + password)
+
